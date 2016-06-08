3646212e696d9e3fdb0c2a42dd7419acef09b53b
Beau August <beau.august@gforces.co.uk>
2016-06-08 16:41:17 +0100

    update to root folder instead of specific file

--- save&commit/saveCommit.py
+++ save&commit/saveCommit.py
@@ -1,5 +1,6 @@
 import sublime, sublime_plugin
 import subprocess
+import os
 
 class save_commitCommand(sublime_plugin.TextCommand):
 
@@ -8,10 +9,14 @@ class save_commitCommand(sublime_plugin.TextCommand):
 
 	def run(self, edit): 	
 		self.view.run_command("save")
-		script = 'START TortoiseProc.exe /command:commit /path:"' + self.view.file_name() + '"'
+		current_file_path = self.view.file_name()
+		split_file_path = current_file_path.split(os.sep)
+		parent_folder = str.join(os.sep, split_file_path[:split_file_path.index("less")])
+		script = 'START TortoiseProc.exe /command:commit /path:"' + parent_folder + '"'
 		pr = subprocess.Popen(script,
 			shell = True,
 			stdout = subprocess.PIPE,
 			stderr = subprocess.PIPE,
 			stdin = subprocess.PIPE)
 		(result, error) = pr.communicate()
+		strings = ''
