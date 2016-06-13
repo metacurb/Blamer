ee9a1d5df4790920b0e086db831abd6b5ddb37e4
unknown <beauagst@gmail.com>
2016-06-13 22:29:48 +0100

    more settings added for auto-blaming based on file format, and setting trunk folder

--- Blamer.py
+++ Blamer.py
@@ -5,6 +5,26 @@ from .subversion import Subversion
 from .filecacher import FileCacher
 from .config import file_cacher_dict
 
+class auto_blameCommand(sublime_plugin.EventListener):
+	def on_load(self, view):
+		blamer_settings = sublime.load_settings("Blamer.sublime-settings")
+		blame_format = blamer_settings.get("auto_blame_format")
+		auto_blame = blamer_settings.get("auto_blame")
+		blame_root = blamer_settings.get("root_folder")
+		current_file = view.file_name()
+
+		if current_file.find(blame_root) != -1:
+			root_exists = True
+
+		if blame_format == ".*":
+			correct_format = True
+		else:
+			correct_format = current_file.endswith(blame_format)
+
+		if root_exists and auto_blame and correct_format:
+			view.run_command("blame_set")
+
+
 class blame_setCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		file_cacher = None
